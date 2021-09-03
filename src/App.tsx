c0c76ba72f0f0a97dfd6e57ec2d23cec6bb67503
Lincoln Anderson <landerson@threefivetwo.com>
2021-09-03 16:38:56 -0400

    install common libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as UI from '@chakra-ui/layout';
 
-function App() {
+const App: React.FC = () => {
   return (
     <UI.Box p="4">
       <UI.Heading>Hello React!</UI.Heading>
     </UI.Box>
   );
-}
+};
 
 export default App;
